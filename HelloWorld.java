8a1d2404c7370fea868d231dccc04eb80a9efa3c
ljsnell <ljsnell@users.noreply.github.com>
2022-12-31 17:33:07 -0600

    Week 1 examples (#1)
    * Create HelloWorld.java
    
    Example from Moodle
    
    * Create MyClass
    
    Additional Example

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Have a good day!");
+	}
+
+}
+
+/*
+You may or may not have this program as part of a package.
+Everything in Java is done in terms of Class Structures.
+A Class is a collection of data and procedures (called methods).
+The command "public" indicates that something is available to other classes and method.
+"Public" would be in contrast with "Private."
+"Static" indicates that there is only one of something no matter how many objects 
+ of the same type are instantiated.
+For examples one could instantiate "Hello World" multiple times, 
+ but there would only be one "main" for them all.
+"void" indicates that there is no output or input.  
+   In this case the void indicates that there is no output to another class.
+   Of course there is output to the console.
+"String[] args" indicates that the inputs to the mainline should be 
+   stored in an array of strings.  
+In this case there would actually be no inputs to the mainline.
+"System.out.println" indicates that the method println comes from the class "System"
+  with subclass "out".
+*/
